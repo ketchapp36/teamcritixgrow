a4c8e41c739568f9fb1ffd80cda0288fd756dd44
ketchapp36 <55000931+ketchapp36@users.noreply.github.com>
2019-09-07 15:26:04 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const fs = require('fs');
+let config = require('./botconfig.json');
+let token = config.token;
+let prefix = config.prefix;
+
+fs.readdir('./cmds/',(err,files)=>{
+    if(err) console.log(err);
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
+    console.log(`Загружено ${jsfiles.length} комманд`);
+    jsfiles.forEach((f,i) =>{
+        let props = require(`./cmds/${f}`);
+        console.log(`${i+1}.${f} Загружен!`);
+        bot.commands.set(props.help.name,props);
+    })
+})
+
+bot.on('ready', () => {
+  console.log(`Запустился бот ${bot.user.username}`);
+  bot.user.setActivity("!help");
+  bot.generateInvite(["ADMINISTRATOR"]).then(link =>{
+      console.log(link);
+  })
+});
+
+bot.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.type == "dm") return;
+    let user = message.author.username;
+    let userid = message.author.id;
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0].toLowerCase();
+    let args = messageArray.slice(1);
+    if(!message.content.startsWith(prefix)) return;
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot,message,args);
+});
+
+bot.on('guildMemberAdd', (guildMember) => {
+    guildMember.addRole(guildMember.guild.roles.find(role => role.name === "👁‍🗨 Участник👁‍🗨"));
+});
+
+bot.login(token);
